76d0dba627fde327c0a4d8054903fc57b372cad7
Oksana Zelenevskaya <exoticisl@gmail.com>
2021-03-04 01:23:21 +0200

    Добавляет задачи

--- js/02module-hw-2.js
+++ js/02module-hw-2.js
@@ -39,17 +39,39 @@ function isLoginUnique (allLogins, login) {
   return allLogins.includes(login);
 }
 
-function addLogin(allLogins, login, isLoginValid, isLoginUnique) {
+function addLogin(allLogins, login) {
   const SUCCESS = 'Логин успешно добавлен!';
   const REFUSAL = 'Такой логин уже используется!';
   const ERROR = 'Ошибка! Логин должен быть размером от 4 до 16 символов';
-  console.log(isLoginValid());
-  
+  let message = ERROR;
+  for (const log of allLogins) {
+    // console.log(login);
+    const valid = isLoginValid(log);
+    const unique = isLoginUnique(log)
+    if (valid) {
+      if (unique) {
+        message = SUCCESS;
+      }
+        message = REFUSAL;  
+    }
+    return message;
+  }
+// 
+//   if (!isLoginValid(login)) {
+//     message = ERROR;
+//   } else if (!isLoginUnique(login)) {
+//     allLogins.push(login);
+//     message = REFUSAL;
+//   } else {
+//     message = SUCCESS;
+//   }
+  return message;
 }
+
 
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 
-console.log(addLogin(logins, 'Ajax')); 
+// console.log(addLogin(logins, 'Ajax')); 
 // 'Логин успешно добавлен!
 
 // console.log(addLogin(logins, 'robotGoogles')); 
@@ -60,4 +82,38 @@ console.log(addLogin(logins, 'Ajax'));
 
 // console.log(addLogin(logins, 'jqueryisextremelyfast')); 
 // 'Ошибка! Логин должен быть от 4 до 16 символов' 
+
+const filter = function (array, test) {
+    const filteredArray = [];
+
+    for (const el of array) {
+        console.log(el);
+        const passed = test(el);
+
+        if (passed) {
+            filteredArray.push(el);
+        }
+    }
+
+    return filteredArray;
+};
+
+// 1. надо передать функцию
+// 2. функция получает элемент массива
+// 3. если элемент массива удовлетворяет условию то функция вернет true
+// 3. если элемент массива НЕ удовлетворяет условию то функция вернет false
+
+// const callback1 = function (value) {
+//     return value >= 3;
+// };
+
+// const r1 = filter([1, 2, 3, 4, 5], callback1);
+// console.log(r1);
+
+// const callback2 = function (value) {
+//     return value <= 4;
+// };
+
+// const r2 = filter([1, 2, 3, 4, 5, 6, 7, 8], callback2);
+// console.log(r2);
 
